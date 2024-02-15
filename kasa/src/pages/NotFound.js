5c9aef880dc9f55fb6d019f2daf150f96253671b
Thomas Fantou <thomasfantoupro@gmail.com>
2024-02-15 16:16:29 +0100

    Création de la page NotFound (en révision)

--- kasa/src/pages/NotFound.js
+++ kasa/src/pages/NotFound.js
@@ -1,11 +1,19 @@
 // NotFound.js
 import React from 'react';
+import '../scss/NotFound.scss'; // Import des styles SCSS pour NotFound
 
 const NotFound = () => {
   return (
-    <div>
-      <h2>Page non trouvée</h2>
-      <p>Désolé, la page que vous recherchez est introuvable.</p>
+    <div className="D404"> {/* Utilisation de la classe D404 */}
+      <div className="Element404"> {/* Utilisation de la classe Element404 */}
+        <p className="Text notFoundText">404</p> {/* Utilisation de la classe Text pour le style du texte */}
+      </div>
+      <div className="errorMessage-container">
+        <p className="Text errorMessage">Oups! La page que vous demandez n'existe pas.</p> {/* Utilisation de la classe Text pour le style du texte */}
+      </div>
+      <div className="returnText-container">
+        <p className="Text returnText">Retourner sur la page d’accueil</p> {/* Utilisation de la classe Text pour le style du texte */}
+      </div>
     </div>
   );
 };
