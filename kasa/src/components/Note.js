87a0e444880773df6dfa9e58a45be3baeb507bc6
Thomas Fantou <thomasfantoupro@gmail.com>
2024-02-21 08:00:30 +0100

    Grandes avancées, page FicheLogement sur la fin.

--- kasa/src/components/Note.js
+++ kasa/src/components/Note.js
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../scss/Note.scss';
-import '../assets/etoilepleine.svg'
-import '../assets/etoilevide.svg';
+import etoilePleine from '../assets/etoilepleine.svg';
+import etoileVide from '../assets/etoilevide.svg';
+import { logementList } from '../data/logementList'; // Importez logementList.js
 
 const Note = () => {
+  const [etoiles, setEtoiles] = useState([false, false, false, false, false]);
+
+  useEffect(() => {
+    // Recherchez l'objet logement avec l'identifiant spécifié
+    const logement = logementList.find(logement => logement.identifiant === "c67ab8a7");
+    if (logement) {
+      // Extrait la valeur de la propriété "note"
+      const note = parseInt(logement.note); // Convertit la note en nombre
+      if (!isNaN(note) && note >= 1 && note <= 5) {
+        // Met à jour l'état des étoiles en fonction de la note
+        setEtoiles(prevEtoiles => {
+          const newEtoiles = new Array(5).fill(false); // Initialiser toutes les étoiles à vide
+          for (let i = 0; i < note; i++) {
+            newEtoiles[i] = true; // Marquer les étoiles jusqu'à la note comme pleines
+          }
+          return newEtoiles;
+        });
+      }
+    }
+  }, []); // Exécuté une seule fois après le rendu initial
+
   return (
     <div className="star-container">
-        <div className="star_full">
-
-        </div>
-        <div className="star_empty">
-
-        </div>
+      {etoiles.map((pleine, index) => (
+        <img
+          key={index}
+          src={pleine ? etoilePleine : etoileVide}
+          alt={pleine ? 'etoile pleine' : 'etoile vide'}
+          className="star"
+        />
+      ))}
     </div>
   );
 };
--- kasa/src/pages/FicheLogement.js
+++ kasa/src/pages/FicheLogement.js
@@ -1,37 +1,53 @@
-// FicheLogement.js
 import React from 'react';
 import Caroussel from '../components/Caroussel';
 import BackgroundSVG from '../assets/accomodation.svg';
 import Collapse from '../components/Collapse';
 import '../scss/FicheLogement.scss';
 import Tag from '../components/Tag';
-import Note from '../components/Note'; // Importez le composant Note
+import Note from '../components/Note';
+import RoundImage from '../components/RoundImage'; // Importer le composant RoundImage
+import { logementList } from '../data/logementList'; // Importer les données des logements
 
 const FicheLogement = () => {
-  // Calcul de la largeur disponible pour le Caroussel
-  const pageWidth = '100vw'; // Largeur totale de la page
-  const pageMargin = '5vw'; // Marge de 5% à droite et à gauche
-  const availableWidth = `calc(${pageWidth} - 2 * ${pageMargin})`; // Largeur disponible pour le Caroussel
+  const pageWidth = '100vw';
+  const pageMargin = '5vw';
+  const availableWidth = `calc(${pageWidth} - 2 * ${pageMargin})`;
+
+  // Sélectionner un logement spécifique (par exemple, avec identifiant "1e180563")
+  const logement = logementList.find(logement => logement.identifiant === "1e180563");
 
   return (
     <div className="fiche-logement">
       <div className="content-wrapper">
-        <Caroussel images={[BackgroundSVG]} availableWidth={availableWidth} />
+        <Caroussel images={logement && logement["des photos"]} availableWidth={availableWidth} />
         <div className="title-wrapper">
-          <h1 class="titre1">Cozy loft on the Canal Saint-Martin</h1>
-          <h2 class="titre2">Paris, Île-de-France</h2>
+          <div className="host-info-wrapper">
+            <h1 className="titre1">Cozy loft on the Canal Saint-Martin</h1>
+            <div className="text-container">
+              <p>{logement && logement.hôte.nom}</p> {/* Afficher le nom de l'hôte */}
+            </div>
+            <div className="profile-pic-container">
+              <RoundImage imageUrl={logement && logement.hôte.photo} altText={logement && logement.hôte.nom} /> {/* Afficher la photo de profil de l'hôte */}
+            </div>
+          </div>
+          <h2 className="titre2">Paris, Île-de-France</h2>
           <div className='tag-zone'>
             <Tag title={"Cozy"} />
             <Tag title={"Canal"} />
             <Tag title={"Paris 10"} />
-            <div className="note-container">
+            <div className='note-container'>
               <Note />
             </div>
           </div>
         </div>
+
         <div className="collapse-wrapper">
-          <Collapse id="cb2f9222" />
-          <Collapse id="d60ca600" />
+          <div className="collapse-container">
+            <Collapse id="cb2f9222" />
+          </div>
+          <div className="collapse-container">
+            <Collapse id="d60ca600" />
+          </div>
         </div>
       </div>
     </div>
