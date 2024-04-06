80e92ca122d9111bee248f14b48320fe5c5ed00a
Thomas Fantou <thomasfantoupro@gmail.com>
2024-04-06 05:52:11 +0200

    Responsive : Mobile. Footer pré-modifications supplémentaires.

--- kasa/src/components/Card.js
+++ kasa/src/components/Card.js
@@ -7,10 +7,12 @@ import '../scss/Card.scss';
 function Card({ id, image, title }) {
   return (
     <div className="card">
-      <Link to={`/fichelogement/${id}`} className="card_img_container">
-        <img src={image} alt={title} className="card_img" />
+      <div className="card_img_container">
+        <Link to={`/fichelogement/${id}`}>
+          <img src={image} alt={title} className="card_img" />
+        </Link>
         <h2 className="card_title"><span className="title_text">{title}</span></h2>
-      </Link>
+      </div>
     </div>
   );
 }
