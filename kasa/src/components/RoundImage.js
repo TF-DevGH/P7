8b34b4d26fe51140a196b4aa282a597799d791af
Thomas Fantou <thomasfantoupro@gmail.com>
2024-05-16 02:04:06 +0200

    Post mentorat. Ajustements mineurs à venir (0/1). Repositionnement des divs .text-container / .profile-pic-container / .note-container après les ajustements mineurs (0/3).

--- kasa/src/components/RoundImage.js
+++ kasa/src/components/RoundImage.js
@@ -5,7 +5,7 @@ import { logementList } from '../data/logementList'; // Importez logementList.js
 const RoundImage = ({ imageUrl, altText }) => {
     return (
       <div className="round-image-container">
-        <img src={imageUrl} alt={altText} className="profile-image" style={{ width: '120px', height: '120px' }} /> {/* Ajustez la taille selon vos besoins */}
+        <img src={imageUrl} alt={altText} className="profile-image"/> {/* Ajustez la taille selon vos besoins */}
       </div>
     );
 };
