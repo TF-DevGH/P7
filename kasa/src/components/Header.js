cab733be2702c30bd6706d393992666c5a76d380
TF-DevGH <thomasfantoupro@gmail.com>
2024-06-06 15:07:02 +0200

    Peaufinage de FicheLogement (Mobile / Desktop). Travail à faire sur le Header. Save assez aléatoire, je vais sortir en voiture.

--- kasa/src/components/Header.js
+++ kasa/src/components/Header.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import '../scss/Header.scss'; // Importer les styles SCSS pour la version PC
-import '../scss/Header_mobile.scss'; // Importer les styles SCSS pour la version mobile
+import '../scss/Header.scss'; // Importer les styles SCSS pour la version PC et mobile
 import LogoSVG from '../assets/logoKasa.svg'; // Importer le fichier SVG du logo
 
 const Header = () => {
-  // Fonction pour rediriger vers /home
   const goToHome = () => {
     window.location.href = '/home';
   };
 
-  // Fonction pour rediriger vers /about
   const goToAbout = () => {
     window.location.href = '/about';
   };
@@ -17,11 +14,10 @@ const Header = () => {
   return (
     <header>
       <div className="container">
-        <div className="header-logo"> {/* Changez la classe en header-logo */}
-          <img class="KasaLogo" src={LogoSVG} alt="Kasa Logo"/>
+        <div className="header-logo">
+          <img className="KasaLogo" src={LogoSVG} alt="Kasa Logo"/>
         </div>
         <nav className="menu">
-          {/* Utiliser les fonctions pour effectuer la navigation */}
           <h1 className="menu-item accueil" onClick={goToHome}>Accueil</h1>
           <h1 className="menu-item apropos" onClick={goToAbout}>A Propos</h1>
         </nav>
